f9357dcac94bd2cf50509ff496d2a793ace94c87
Yumat <mr.yuma.tanaka@gmail.com>
2020-09-09 19:41:40 -0400

    explicitly added axios baseurl

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import logo from "./logo.svg";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
+import axios from "axios";
 
 //Pages
 import home from "./pages/home";
 import blog from "./pages/blog";
+
+axios.defaults.baseURL = "https://testblogbackendyuma.herokuapp.com";
 
 function App() {
   return (
